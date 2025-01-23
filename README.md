6a50b10c10eba8325908f7cb69d7e3fdaeaff928
Oky Aneka <okyaneka@gmail.com>
2025-01-23 23:46:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Nuxt Minimal Starter
+
+Look at the [Nuxt documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
+
+## Setup
+
+Make sure to install dependencies:
+
+```bash
+pnpm install
+```
+
+## Development Server
+
+Start the development server on `http://localhost:1236`:
+
+```bash
+pnpm dev
+```
+
+## Production
+
+Build the application for production:
+
+```bash
+pnpm build
+```
+
+Locally preview production build:
+
+```bash
+pnpm preview
+```
---
+++ nuxt.config.ts
@@ -0,0 +1,44 @@
+import { env, tailwindcss, icon } from "./_src/configs";
+import fs from "fs";
+import path from "path";
+import font from "./_src/configs/font";
+
+// get data from package.json
+const packagejson: any = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "package.json"), "utf-8")
+);
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  watch: ["./_src/configs/**/*"],
+  css: ["@/main.css"],
+  components: [
+    { prefix: "C", path: "./_src/components/", extensions: ["vue"] },
+  ],
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: env.APP_NAME,
+      meta: [{ name: "description", content: packagejson.description }],
+    },
+
+    layoutTransition: { name: "fade", mode: "out-in" },
+    pageTransition: { name: "fade", mode: "out-in" },
+  },
+  devServer: { port: env.PORT, host: "0.0.0.0" },
+  alias: {
+    "~src": "./_src",
+  },
+  runtimeConfig: {
+    public: env,
+  },
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    ["@nuxtjs/tailwindcss", tailwindcss],
+    ["@nuxtjs/google-fonts", font],
+    ["@nuxt/icon", icon],
+    "@vueuse/nuxt",
+  ],
+});
