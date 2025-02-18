6c030855f4384070ab349495691e073640fa36a8
Oky Aneka <okyaneka@gmail.com>
2025-02-18 20:21:43 +0700

    feat: add error handling for QR scanning and improve user feedback

--- pages/scan/index.vue
+++ pages/scan/index.vue
@@ -10,19 +10,16 @@ definePageMeta({
 
 const router = useRouter();
 
-const { camera } = toRefs(useConfigStore());
-
-const mediaRef = ref<HTMLVideoElement>();
-const inputRef = ref<HTMLInputElement>();
-const stream = ref<MediaStream>();
-const devices = ref<MediaDeviceInfo[]>();
 const result = ref("");
-const isTorch = ref(false);
-const isTorchOn = ref(false);
-const isLoaded = ref(false);
-const isDenied = ref<boolean>();
 
-const { data, refetch: verify, isLoading } = qr.verify(result);
+const isError = ref(false);
+
+const {
+  data,
+  refetch: verify,
+  isLoading,
+  isError: queryError,
+} = qr.verify(result);
 
 const onVerify = (qrString: string) => {
   result.value =
@@ -32,141 +29,6 @@ const onVerify = (qrString: string) => {
       .pop() || "";
   verify();
 };
-
-const setDevice = async () => {
-  if (stream.value)
-    stream.value.getTracks().forEach((track) => {
-      track.stop();
-    });
-
-  const opts = camera.value ? { deviceId: { exact: camera.value } } : true;
-
-  stream.value = await navigator.mediaDevices
-    .getUserMedia({
-      video: opts,
-    })
-    .then(async (res) => {
-      const track = res.getTracks()[0];
-      const capabilities = track.getCapabilities();
-      const divider = Math.round(
-        // @ts-ignore
-        Math.max(capabilities.height.max, capabilities.width.max) / 1000
-      );
-      await track.applyConstraints({
-        // @ts-ignore
-        height: capabilities.height.max / divider,
-        // @ts-ignore
-        width: capabilities.width.max / divider,
-      });
-
-      // @ts-ignore
-      isTorch.value = !!capabilities.torch;
-      return res;
-    });
-};
-
-const startScanning = () => {
-  if (!stream.value || !mediaRef.value) return;
-  result.value = "";
-  qrcode.scanByStream(stream.value).then((res) => {
-    onVerify(res.getText());
-    setTimeout(() => {
-      startScanning();
-    }, 1e3);
-  });
-};
-
-const getPermission = async () => {
-  const permission = await navigator.permissions.query({ name: "camera" });
-  if (permission.state === "denied") return (isDenied.value = true);
-  if (permission.state === "prompt")
-    await navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-      })
-      .then((stream) => {
-        const track = stream.getTracks()[0];
-        track.stop();
-      })
-      .catch((err) => {
-        isDenied.value = true;
-        throw new err();
-      });
-
-  if (!camera.value) await getDevices();
-};
-
-const getDevices = async () => {
-  devices.value = (await navigator.mediaDevices.enumerateDevices()).filter(
-    (v) => v.kind === "videoinput"
-  );
-
-  for (const device of devices.value) {
-    const stream = await navigator.mediaDevices
-      .getUserMedia({
-        video: { deviceId: { exact: device.deviceId } },
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-    if (!stream) continue;
-    const track = stream.getTracks()[0];
-    const capabilities = track.getCapabilities();
-    camera.value = device.deviceId;
-    track.stop();
-
-    if (
-      device.label.toLowerCase().includes("back") &&
-      // @ts-ignore
-      capabilities.focusMode &&
-      // @ts-ignore
-      (capabilities.focusMode as string[]).includes("continuous")
-    )
-      return;
-  }
-};
-
-const toggleTorch = async () => {
-  if (!stream.value) return;
-  const track = stream.value.getTracks()[0];
-
-  isTorchOn.value = !isTorchOn.value;
-  // @ts-ignore
-  await track.applyConstraints({ torch: isTorchOn.value });
-};
-
-const stopTrack = async () => {
-  if (stream.value) stream.value.getTracks().forEach((track) => track.stop());
-};
-
-const onInputChange = async () => {
-  if (!inputRef.value) return;
-  const { files } = inputRef.value;
-  if (!files) return;
-
-  qrcode
-    .scanByFile(files[0])
-    .then((res) => {
-      onVerify(res.getText());
-    })
-    .catch((e) => {
-      toast.push(`qr code cannot be found`, { type: "error" });
-    });
-  inputRef.value.type = "text";
-  await nextTick();
-  inputRef.value.type = "file";
-};
-
-const handleRefresh = () => {
-  location.reload();
-};
-
-watch([stream, mediaRef], async () => {
-  if (stream.value && mediaRef.value) {
-    mediaRef.value.srcObject = stream.value;
-    startScanning();
-  }
-});
 
 watch(data, () => {
   if (!data.value) return;
@@ -180,21 +42,53 @@ watch(data, () => {
   }
 });
 
-onMounted(() => {
-  getPermission()
-    .then(() => setDevice())
-    .then(() => (isLoaded.value = true));
-});
-
-onUnmounted(() => {
-  stopTrack();
+watch(queryError, () => {
+  isError.value = queryError.value;
 });
 </script>
 
 <template>
-  <CQrScanner class="h-screen w-full" from-file @scanned="onVerify">
-    <CBarTitle :back="routes.index" variant="light">
+  <div class="flex flex-col gap-4 min-h-[calc(100vh)] relative">
+    <CBarTitle
+      :back="routes.index"
+      class="z-[9999] absolute top-0 left-0 w-full"
+      variant="light"
+    >
       <CChip class="text-base font-normal"> Scan QR Disini </CChip>
     </CBarTitle>
-  </CQrScanner>
+
+    <CQrScanner
+      class="h-screen w-full"
+      :disabled="isError"
+      from-file
+      @scanned="onVerify"
+    >
+      <div class="h-full w-full relative">
+        <Transition name="fade">
+          <div
+            v-if="isError"
+            class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full p-4 flex justify-center"
+          >
+            <CCard class="max-w-full w-80" content-class="flex flex-col gap-2">
+              <div>
+                <div class="flex justify-center">
+                  <Icon class="text-red-500" name="ri:close-line" size="48" />
+                </div>
+
+                <div class="text-xl font-bold text-center">
+                  Ups. Kode QR nya tidak valid!
+                </div>
+              </div>
+
+              <div class="text-center mt-4">
+                <CButton color="light" @click="isError = false">
+                  Cari Lagi!
+                </CButton>
+              </div>
+            </CCard>
+          </div>
+        </Transition>
+      </div>
+    </CQrScanner>
+  </div>
 </template>
