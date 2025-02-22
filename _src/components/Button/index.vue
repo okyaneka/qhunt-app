e31c33eaf71820ab4b16e3d7acccc13bb459a064
Oky Aneka <okyaneka@gmail.com>
2025-02-22 14:27:11 +0700

    feat: enhance Button component with new props and hover overlay effect

--- _src/components/Button/index.vue
+++ _src/components/Button/index.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from "vue";
-import type { RouterLinkProps } from "vue-router";
+import type { ButtonHTMLAttributes, HTMLAttributes } from "vue";
+import { RouterLink, type RouterLinkProps } from "vue-router";
 
-useAttrs();
+type Props = {
+  as: "link" | "button";
+  color: "default" | string;
+  variant: "default" | "light" | "flat";
+  size: "sm" | "md" | "lg";
+  disabled: boolean;
+  bordered: boolean;
+  icon: boolean;
+  prependIcon: string;
+  appendIcon: string;
+} & RouterLinkProps;
+
+const attrs = useAttrs() as Partial<ButtonHTMLAttributes>;
 
 const {
   as = "button",
@@ -11,36 +23,33 @@ const {
   size = "md",
   disabled,
   icon,
+  prependIcon,
+  appendIcon,
+  bordered,
   ...props
-} = defineProps<
-  Partial<RouterLinkProps> & {
-    as?: "link" | "button";
-    color?: "default" | "light";
-    variant?: "default" | "light" | "flat";
-    size?: "sm" | "md" | "lg";
-    disabled?: boolean;
-    icon?: boolean;
-  }
->();
+} = defineProps<Partial<Props>>();
+
+const linkAttrs = computed(() => ({
+  ...props,
+  class: attrs.class,
+  style: attrs.style,
+  to: props.to || "",
+}));
 
 const setVariant = () => {
   switch (variant) {
     case "light":
       return `bg-dark ${
-        disabled
-          ? "text-opacity-50 bg-opacity-20"
-          : "bg-opacity-30 hover:bg-dark hover:bg-opacity-20 active:bg-opacity-30"
+        disabled ? "text-opacity-50 bg-opacity-20" : "bg-opacity-30"
       } `;
     case "flat":
       return `bg-dark bg-opacity-0 text-dark ${
-        disabled ? "text-opacity-50" : "hover:bg-opacity-5 active:bg-opacity-10"
+        disabled ? "text-opacity-50" : ""
       } `;
 
     default:
       return `bg-dark text-white ${
-        disabled
-          ? "bg-opacity-50 text-opacity-50"
-          : "hover:bg-opacity-90 active:bg-opacity-80"
+        disabled ? "bg-opacity-50 text-opacity-50" : ""
       } `;
   }
 };
@@ -50,9 +59,9 @@ const setSize = () => {
     case "lg":
       return icon ? "h-14 w-14 text-xl " : "min-h-14 px-4 py-2 text-xl ";
     case "sm":
-      return icon ? "h-7 w-7 text-sm " : "min-h-7 px-2 py-0.5 text-sm ";
+      return icon ? "h-7 w-7 text-md " : "min-h-7 px-2 py-0.5 text-sm ";
     default:
-      return icon ? "h-10 w-10 " : "min-h-10 px-4 py-2 ";
+      return icon ? "h-10 w-10 text-lg " : "min-h-10 px-4 py-2 ";
   }
 };
 
@@ -63,12 +72,13 @@ const setIsIcon = () => {
 
 const className: HTMLAttributes["class"] = computed(() => {
   let theClass =
-    "button inline-flex justify-center items-center text-center transition select-none font-semibold ";
+    "button hover-overlay overflow-clip inline-flex justify-center items-center text-center transition select-none font-semibold ";
   theClass += setVariant();
   theClass += setIsIcon();
   theClass += setSize();
+  theClass += bordered ? "border " : "";
 
-  if (color == "light") {
+  if (color != "default") {
     theClass = theClass.replaceAll("dark", color);
     if (variant == "default")
       theClass = theClass.replaceAll("text-white", "text-dark");
@@ -79,15 +89,21 @@ const className: HTMLAttributes["class"] = computed(() => {
 </script>
 
 <template>
-  <RouterLink
-    v-if="as === 'link'"
+  <component
+    :is="as === 'link' ? RouterLink : 'button'"
     :class="className"
-    v-bind="{ ...props }"
-    :to="props.to || ''"
+    v-bind="as === 'link' ? linkAttrs : attrs"
   >
-    <slot />
-  </RouterLink>
-  <button v-else :class="className">
-    <slot />
-  </button>
+    <Icon v-if="prependIcon && !icon" class="-ml-1 mr-2" :name="prependIcon" />
+    <div class="inline-flex relative">
+      <slot />
+    </div>
+    <Icon v-if="appendIcon && !icon" class="-mr-1 ml-2" :name="appendIcon" />
+  </component>
 </template>
+
+<style scoped>
+.hover-overlay {
+  @apply relative before:absolute before:inset-0 before:bg-gray-100 before:bg-opacity-0 before:transition-all before:duration-300 before:mix-blend-luminosity before:hover:bg-opacity-30 before:active:bg-gray-500 before:active:bg-opacity-10;
+}
+</style>
