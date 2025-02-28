6c3bf1300abf9984e414aa44890768df0e31a30f
Oky Aneka <okyaneka@gmail.com>
2025-03-01 03:02:38 +0700

    Refactor environment variables to use NUXT_PUBLIC prefix for better clarity

--- _src/configs/env/index.ts
+++ _src/configs/env/index.ts
@@ -1,19 +1,14 @@
 export const env = {
   PORT: Number(process.env.PORT) || 3000,
   APP_NAME: process.env.APP_NAME || "",
-  APP_API_URL: process.env.APP_API_URL || "",
-  APP_SOCKET_URL: process.env.APP_SOCKET_URL || "",
-
-  FIREBASE_KEY: process.env.FIREBASE_KEY || "",
-  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || "",
+  APP_API_URL: "",
+  APP_SOCKET_URL: "",
+  FIREBASE_KEY: "",
+  FIREBASE_AUTH_DOMAIN: "",
 } as const;
 
 export const useEnv = () => {
-  if (process.client) {
-    return useRuntimeConfig().public;
-  } else {
-    return env;
-  }
+  return useRuntimeConfig();
 };
 
 export default env;
