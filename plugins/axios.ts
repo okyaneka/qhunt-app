7c87f5bd9de8bf2c081c71e2c0284f69a8a2919d
Oky Aneka <okyaneka@gmail.com>
2025-03-02 03:01:42 +0700

    fix: refactor axios plugin to set defaults on app creation and improve error handling

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -8,33 +8,38 @@ export default defineNuxtPlugin(() => {
   const authStore = useAuthStore();
   const env = useEnv();
   const router = useRouter();
+  const nuxtApp = useNuxtApp();
 
-  axios.defaults.baseURL = env.public.APP_API_URL;
-  axios.defaults.withCredentials = true;
+  nuxtApp.hook("app:created", () => {
+    axios.defaults.baseURL = env.public.APP_API_URL;
+    axios.defaults.withCredentials = true;
 
-  axios.interceptors.request.use(
-    (config) => config,
-    (error) => Promise.reject(error)
-  );
+    axios.interceptors.request.use(
+      (config) => config,
+      (error) => Promise.reject(error)
+    );
 
-  axios.interceptors.response.use(
-    (res) => res,
-    async (err: AxiosError<DefaultResponse>) => {
-      const res = err.response?.data;
-      if (import.meta.client) {
-        const message = res?.message || err.message;
-        if (res && message == "validation_error") {
-          const validation = Object.values(res.error?.validation || {}) as any;
-          push(validation[0], { type: "error" });
-        } else push(message, { type: "error" });
+    axios.interceptors.response.use(
+      (res) => res,
+      async (err: AxiosError<DefaultResponse>) => {
+        const res = err.response?.data;
+        if (import.meta.client) {
+          const message = res?.message || err.message;
+          if (res && message == "validation_error") {
+            const validation = Object.values(
+              res.error?.validation || {}
+            ) as any;
+            push(validation[0], { type: "error" });
+          } else push(message, { type: "error" });
 
-        if (res?.code == 401) {
-          authStore.auth = undefined;
-          await useFetch("/api/me", { credentials: "include" });
-          router.push(routes.login);
+          if (res?.code == 401) {
+            authStore.auth = undefined;
+            await useFetch("/api/me", { credentials: "include" });
+            router.push(routes.login);
+          }
         }
+        return Promise.reject(err);
       }
-      return Promise.reject(err);
-    }
-  );
+    );
+  });
 });
