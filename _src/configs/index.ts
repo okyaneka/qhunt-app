fdb7361b922d18dba9419c9fc26b63f3f8374b61
Oky Aneka <okyaneka@gmail.com>
2025-03-02 23:31:40 +0700

    feat: add PWA support with icons and splash screens

--- _src/configs/index.ts
+++ _src/configs/index.ts
@@ -1,16 +1,18 @@
 import env from "./env";
 import font from "./font";
 import icon from "./icon";
+import pwa from "./pwa";
 import ssr from "./ssr";
 import tailwindcss from "./tailwindcss";
 
-export { env, tailwindcss, icon, ssr, font };
+export { env, tailwindcss, icon, ssr, font, pwa };
 
 const configs = {
   env,
   tailwindcss,
   icon,
   ssr,
+  pwa,
   font,
 } as const;
 
