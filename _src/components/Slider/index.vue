43fc4ff41807c706c5b537788a1c309b950201c2
Oky Aneka <okyaneka@gmail.com>
2025-03-03 20:22:28 +0700

    feat: integrate Swiper for enhanced slider functionality

--- _src/components/Slider/index.vue
+++ _src/components/Slider/index.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/scrollbar";
+
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Autoplay, Scrollbar } from "swiper/modules";
 import { RouterLink } from "vue-router";
 import type { SliderItem } from "~/_src/helpers";
+import type { Swiper as SwiperInstance } from "swiper/types";
 
 type Props = {
   items: SliderItem[];
@@ -23,15 +30,8 @@ const {
 } = defineProps<Partial<Props>>();
 const slots = useSlots();
 
-const sliderRef = ref<HTMLElement>();
-const slideContentRef =
-  ref<(HTMLElement | InstanceType<typeof RouterLink>)[]>();
-const isTouched = ref(false);
-const isPause = ref(false);
-const quickActiveIndex = ref(0);
-const activeIndex = ref(0);
-const slideInterval = ref<NodeJS.Timeout>();
-const slideTimeout = ref<NodeJS.Timeout>();
+const swiper = ref<SwiperInstance>();
+
 const slideContentHeight = ref(0);
 
 const slotItems = computed(() =>
@@ -47,120 +47,44 @@ const visibleItems = computed<number[]>(() => {
   return Array.from({ length: itemsLength.value }, (_, i) => i);
 });
 
-const center = computed(() => Math.floor(visibleItems.value.length / 2));
-
-const sortedVisibleItems = computed(() => {
-  const rest = [...visibleItems.value];
-  const index = (activeIndex.value + 1 + center.value) % itemsLength.value;
-  const spliced = rest.splice(index);
-  return [...spliced, ...rest];
-});
-
-const scrollWidth = computed(() => {
-  const slider = sliderRef.value;
-  if (!slider) return 0;
-  const { gap } = getComputedStyle(slider);
-
-  return slider.clientWidth + parseInt(gap);
-});
-
-const defaultPos = computed(() => {
-  return scrollWidth.value * center.value;
-});
-
 const onPlay = (evt?: TouchEvent | MouseEvent) => {
-  if (evt?.type === "touchend") isTouched.value = false;
   onPause(evt);
-  isPause.value = false;
-  slideInterval.value = setInterval(() => {
-    onNext();
-  }, interval);
+  swiper.value?.autoplay.start();
 };
 
 const onPause = (evt?: TouchEvent | MouseEvent) => {
-  if (evt?.type === "touchstart") isTouched.value = true;
-  isPause.value = true;
-  clearInterval(slideInterval.value);
+  swiper.value?.autoplay.pause();
 };
 
 const onSetPos = (pos: number) => {
-  if (sliderRef.value) {
-    quickActiveIndex.value = pos;
-    const index = sortedVisibleItems.value.indexOf(pos) - center.value;
-    sliderRef.value.scrollTo({
-      left: defaultPos.value + index * scrollWidth.value,
-      behavior: "smooth",
-    });
-  }
+  swiper.value?.slideTo(pos);
 };
 
 const onNext = () => {
-  if (sliderRef.value) {
-    quickActiveIndex.value = (activeIndex.value + 1) % itemsLength.value;
-    sliderRef.value.scrollTo({
-      left: defaultPos.value + scrollWidth.value,
-      behavior: "smooth",
-    });
-  }
+  swiper.value?.slideNext() || onSetPos(0);
 };
 
 const onPrev = () => {
-  if (sliderRef.value) {
-    quickActiveIndex.value =
-      (activeIndex.value - 1 + itemsLength.value) % itemsLength.value;
-    sliderRef.value.scrollTo({
-      left: defaultPos.value - scrollWidth.value,
-      behavior: "smooth",
-    });
-  }
+  swiper.value?.slidePrev() || onSetPos(itemsLength.value - 1);
 };
 
-const sliderOnScroll = (event: Event) => {
-  if (isTouched.value) return;
-  clearTimeout(slideTimeout.value);
-  slideTimeout.value = setTimeout(() => {
-    const slider = sliderRef.value;
-    if (!slider) return;
-    if (slider.scrollLeft == defaultPos.value) return;
-    const adder = (slider.scrollLeft - defaultPos.value) / scrollWidth.value;
-
-    activeIndex.value =
-      (activeIndex.value + adder + itemsLength.value) % itemsLength.value;
-    quickActiveIndex.value = activeIndex.value;
-  }, 3e2);
-};
-
-const init = async () => {
-  const slider = sliderRef.value;
-  if (slider) {
-    slider.scrollTo({ left: defaultPos.value });
-    slider.addEventListener("scroll", sliderOnScroll);
-  }
-  onPlay();
-};
-
-const uninit = () => {
-  sliderRef.value?.removeEventListener("scroll", sliderOnScroll);
-  clearTimeout(slideTimeout.value);
-  clearInterval(slideInterval.value);
+const onSwiper = (instance: SwiperInstance) => {
+  swiper.value = instance;
 };
 
 watch(
-  quickActiveIndex,
-  async (index) => {
-    if (import.meta.client) {
+  () => swiper?.value?.activeIndex,
+  async (value) => {
+    if (import.meta.client && value !== undefined) {
       await nextTick();
       const el = document.querySelectorAll(".slide-content");
-      const activeEl = el[center.value + 1];
+      const activeEl = el[value];
+
       if (activeEl) slideContentHeight.value = activeEl.clientHeight;
     }
   },
   { immediate: true }
 );
-
-onMounted(() => init());
-
-onUnmounted(() => uninit());
 </script>
 
 <template>
@@ -171,9 +95,15 @@ onUnmounted(() => uninit());
     @touchstart.stop="onPause"
     @touchend="onPlay"
   >
-    <div ref="sliderRef" class="slide-container">
-      <template v-for="item in sortedVisibleItems" :key="item">
-        <div
+    <Swiper
+      ref="swiperRef"
+      class="slide-container"
+      :modules="[Autoplay, Scrollbar]"
+      :autoplay="{ delay: interval }"
+      @swiper="onSwiper"
+    >
+      <template v-for="item in visibleItems" :key="item">
+        <SwiperSlide
           class="slide-item"
           :style="{
             height: `${height}px`,
@@ -193,16 +123,16 @@ onUnmounted(() => uninit());
             <h2>{{ items[item].title }}</h2>
             <p>{{ items[item].subtitle }}</p>
           </component>
-        </div>
+        </SwiperSlide>
       </template>
-    </div>
+    </Swiper>
 
     <div v-if="navigation" class="slide-nav">
       <div
         v-for="item in visibleItems"
         :key="item"
         class="slide-nav-item"
-        :class="{ active: item === quickActiveIndex }"
+        :class="{ active: item === swiper?.activeIndex }"
         @click="onSetPos(item)"
       ></div>
     </div>
@@ -238,13 +168,13 @@ onUnmounted(() => uninit());
     scrollbar-width: none;
   }
   .slide-item {
-    @apply snap-start snap-always flex flex-col flex-none w-full bg-cover bg-center select-none;
+    @apply snap-start snap-always flex flex-col flex-none w-full bg-cover bg-center select-none overflow-hidden;
   }
   .slide-content {
     @apply p-3 block bg-dark bg-opacity-50 text-white mt-auto;
   }
   .slide-nav {
-    @apply absolute bottom-2 right-2 max-w-80 flex gap-1 cursor-pointer;
+    @apply absolute bottom-2 right-2 max-w-80 flex gap-1 cursor-pointer z-10;
   }
   .slide-nav-item {
     @apply rounded-full bg-white bg-opacity-50 w-4 h-4 transition hover:bg-opacity-75;
@@ -253,7 +183,7 @@ onUnmounted(() => uninit());
     @apply bg-opacity-100;
   }
   .slide-nav-arrow {
-    @apply absolute w-full left-0 transition-all duration-300;
+    @apply absolute w-full left-0 transition-all duration-300 z-10;
   }
   .slide-nav-prev,
   .slide-nav-next {
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -44,7 +44,7 @@ onMounted(() => {});
 </script>
 
 <template>
-  <div class="flex flex-col gap-4 p-3 pt-0">
+  <div class="flex flex-col gap-4">
     <CCardAlt
       class="rounded-t-none"
       content-class="flex flex-col bg-dark text-white"
@@ -53,107 +53,111 @@ onMounted(() => {});
       <h2 class="text-center">Selamat datang di {{ env.public.APP_NAME }}!</h2>
     </CCardAlt>
 
-    <CCardAlt content-class="p-0">
-      <CSlider
-        class="bg-cover bg-center"
-        :height="200"
-        pause-on-hover
-        :interval="5e3"
-        :style="{}"
-      >
-        <template #item-1>
-          <div
-            class="p-3 h-full flex flex-col justify-center items-center gap-2 relative"
-            :style="{}"
-          >
-            <Icon
-              class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-opacity-10 text-dark text-[320px]"
-              name="ri:sword-line"
-            />
-            <Icon
-              class="absolute left-0 bottom-0 text-opacity-20 text-dark text-[100px]"
-              name="ri:sword-fill"
-            />
-            <Icon
-              class="absolute right-0 bottom-0 text-opacity-20 text-dark text-[100px]"
-              name="ri:sword-fill"
-            />
-            <h2 class="text-center">Mari mulai petualanganmu!</h2>
-            <CButton as="link" :to="routes.stage.index">Tantangan Saya</CButton>
-          </div>
-        </template>
-        <template #item-2>
-          <div
-            class="p-3 h-full flex flex-col justify-center items-center relative"
-            :style="{}"
-          >
-            <Icon
-              class="absolute left-2 bottom-0 text-opacity-20 text-dark text-[100px]"
-              name="ph:treasure-chest"
-            />
-            <Icon
-              class="absolute right-2 bottom-0 text-opacity-20 text-dark text-[100px]"
-              name="ph:treasure-chest"
-            />
-            <h3 class="text-center">
-              Kumpulkan skor sebanyak-banyaknya dan menangkan hadiah
-              menarik!<small class="text-gray-500">*</small>
-            </h3>
-            <RouterLink :to="routes.TERMS">Syarat dan ketentuan</RouterLink>
-          </div>
-        </template>
-        <template #item-3>
-          <div
-            class="p-3 h-full flex flex-col justify-center items-center relative"
-            :style="{}"
-          >
-            <Icon
-              class="absolute left-2 bottom-0 text-opacity-20 text-dark text-[100px]"
-              name="ri:door-closed-fill"
-            />
-            <Icon
-              class="absolute right-2 bottom-0 text-opacity-20 text-dark text-[100px]"
-              name="ri:door-open-fill"
-            />
-            <h3 class="text-center">Tertarik menjadi relawan kami?</h3>
-            <RouterLink :to="routes.CONTACT">Hubungi kami!</RouterLink>
-          </div>
-        </template>
-      </CSlider>
-    </CCardAlt>
+    <div class="flex flex-col gap-4 px-3">
+      <CCardAlt content-class="p-0">
+        <CSlider
+          class="bg-cover bg-center"
+          :height="200"
+          pause-on-hover
+          :interval="5e3"
+          :style="{}"
+        >
+          <template #item-1>
+            <div
+              class="p-3 h-full flex flex-col justify-center items-center gap-2 relative"
+              :style="{}"
+            >
+              <Icon
+                class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-opacity-10 text-dark text-[320px]"
+                name="ri:sword-line"
+              />
+              <Icon
+                class="absolute left-0 bottom-0 text-opacity-20 text-dark text-[100px]"
+                name="ri:sword-fill"
+              />
+              <Icon
+                class="absolute right-0 bottom-0 text-opacity-20 text-dark text-[100px]"
+                name="ri:sword-fill"
+              />
+              <h2 class="text-center">Mari mulai petualanganmu!</h2>
+              <CButton as="link" :to="routes.stage.index"
+                >Tantangan Saya</CButton
+              >
+            </div>
+          </template>
+          <template #item-2>
+            <div
+              class="p-3 h-full flex flex-col justify-center items-center relative"
+              :style="{}"
+            >
+              <Icon
+                class="absolute left-2 bottom-0 text-opacity-20 text-dark text-[100px]"
+                name="ph:treasure-chest"
+              />
+              <Icon
+                class="absolute right-2 bottom-0 text-opacity-20 text-dark text-[100px]"
+                name="ph:treasure-chest"
+              />
+              <h3 class="text-center">
+                Kumpulkan skor sebanyak-banyaknya dan menangkan hadiah
+                menarik!<small class="text-gray-500">*</small>
+              </h3>
+              <RouterLink :to="routes.TERMS">Syarat dan ketentuan</RouterLink>
+            </div>
+          </template>
+          <template #item-3>
+            <div
+              class="p-3 h-full flex flex-col justify-center items-center relative"
+              :style="{}"
+            >
+              <Icon
+                class="absolute left-2 bottom-0 text-opacity-20 text-dark text-[100px]"
+                name="ri:door-closed-fill"
+              />
+              <Icon
+                class="absolute right-2 bottom-0 text-opacity-20 text-dark text-[100px]"
+                name="ri:door-open-fill"
+              />
+              <h3 class="text-center">Tertarik menjadi relawan kami?</h3>
+              <RouterLink :to="routes.CONTACT">Hubungi kami!</RouterLink>
+            </div>
+          </template>
+        </CSlider>
+      </CCardAlt>
 
-    <CCardAlt>
-      <div class="mb-2">
-        <h3>Ikuti Event Saat Ini!</h3>
+      <CCardAlt>
+        <div class="mb-2">
+          <h3>Ikuti Event Saat Ini!</h3>
+        </div>
+        <CSlider
+          :height="200"
+          pause-on-hover
+          navigation
+          arrow
+          :interval="5e3"
+          :items="eventItems"
+        >
+        </CSlider>
+      </CCardAlt>
+
+      <CCardAlt v-if="!$pwa?.isPWAInstalled" decoration>
+        <h4 class="text-center mb-4">
+          Tingkatkan pengalamanmu dengan mengunduh aplikasi progresif kami
+        </h4>
+        <div class="text-center">
+          <CButton color="light" @click="install"> Install Aplikasi </CButton>
+        </div>
+      </CCardAlt>
+
+      <div class="flex justify-center gap-4">
+        <RouterLink :to="routes.TERMS">Syarat dan Ketentuan</RouterLink>
+        <RouterLink :to="routes.PRIVACY">Kebijakan Privasi</RouterLink>
+        <RouterLink :to="routes.ABOUT">Tentang</RouterLink>
       </div>
-      <CSlider
-        :height="200"
-        pause-on-hover
-        arrow
-        :interval="5e3"
-        :items="eventItems"
-        navigation
-      >
-      </CSlider>
-    </CCardAlt>
 
-    <CCardAlt v-if="!$pwa?.isPWAInstalled" decoration>
-      <h4 class="text-center mb-4">
-        Tingkatkan pengalamanmu dengan mengunduh aplikasi progresif kami
-      </h4>
-      <div class="text-center">
-        <CButton color="light" @click="install"> Install Aplikasi </CButton>
+      <div class="mt-auto">
+        <CCopy no-link />
       </div>
-    </CCardAlt>
-
-    <div class="flex justify-center gap-4">
-      <RouterLink :to="routes.TERMS">Syarat dan Ketentuan</RouterLink>
-      <RouterLink :to="routes.PRIVACY">Kebijakan Privasi</RouterLink>
-      <RouterLink :to="routes.ABOUT">Tentang</RouterLink>
-    </div>
-
-    <div class="mt-auto">
-      <CCopy no-link />
     </div>
   </div>
 </template>
