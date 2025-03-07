b1e34d766ac79bddc630b78b277784f86df59526
Oky Aneka <okyaneka@gmail.com>
2025-03-07 16:04:10 +0700

    feat: add new Feature Card and Title components, enhance BaseInput with help text slot

---
+++ _src/components/_locals/Feature/Title/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Breadcrumb } from "~/_src/components/Breadcrumb/index.vue";
+
+type Props = {
+  title: string;
+  breadcrumb: Breadcrumb[];
+};
+
+const { title, breadcrumb } = defineProps<Partial<Props>>();
+</script>
+
+<template>
+  <div>
+    <CBreadcrumb v-if="breadcrumb" class="mb-2" :breadcrumb="breadcrumb" />
+    <slot v-if="$slots.default" />
+    <h1 v-else>{{ title || "Judul" }}</h1>
+  </div>
+</template>
